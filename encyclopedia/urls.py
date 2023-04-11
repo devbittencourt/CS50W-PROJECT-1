988def76d34d3c7e6a3be1b97405403d3794bf5a
devbittencourt0 <devbittencourt0@gmail.com>
2023-04-11 12:53:33 -0300

    first commit

---
+++ encyclopedia/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("wiki/<str:entry>", views.wiki, name="wiki"),
+    path("search/", views.search, name="search"),
+    path("new", views.new, name="new"),
+    path("random", views.random_entry, name="random_entry"),
+    path("edit/<str:entry>", views.edit, name="edit"),
+    path("<str:entry>", views.error, name="error"),
+    
+]
+
---
+++ entries/test.md
@@ -0,0 +1,7 @@
+#Test
+
+<br>
+
+<h3>testeee 223321556</h3>
+
+<p>testada</p>
